c4a4fa9da524199fa55a776c53226d2b328e7266
Arthur Colle <hello@arthurcolle.com>
2015-05-16 23:49:01 -0400

    Change db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,8 @@
 
 ActiveRecord::Schema.define(version: 20150517014230) do
 
-  create_table "pages", force: true do |t|
-    t.string   "home"
-    t.string   "about"
-    t.string   "contact"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
     t.string   "email"
@@ -38,7 +33,7 @@ ActiveRecord::Schema.define(version: 20150517014230) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
