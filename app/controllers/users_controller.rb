6d6c2683d485181d37fcd20b36ef1fb1e7eb20d9
Arthur Colle <hello@arthurcolle.com>
2015-05-26 20:40:38 -0400

    The partial for _dishes.html.erb 'View dishes' off of the map now includes image tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,9 @@ class UsersController < ActionController::Base
 		respond_to do |format|   
 			format.json {      # listen for json post request
 				@user = User.find(params["id"])
-				# puts "abcxyz"
-				# puts params
 				@user.update(lat: params["lat"], long: params["long"] )
 	        	@user.save
 			    head :no_content # Send a no response to whoever sent the post.
-				
 			}
 			format.html {
 				# Normal form post request (form_for or w.e.)
