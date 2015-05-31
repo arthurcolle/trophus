b439a269fffeb0be9093e9095ed934ab0010a9be
Arthur Colle <hello@arthurcolle.com>
2015-05-31 04:33:25 -0400

    Improved search UI

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -42,9 +42,17 @@ $(function() {
   });
 });
 
-$(function() {
-    $( "#eas_upper" ).draggable();
-  });
+var face_array = [
+'http://becomingthebboss.com/wp-content/uploads/2014/03/circle-brigid-professional.jpg',
+'http://procopyservices.com/wp-content/uploads/2015/02/profile-circle.png',
+'http://www.makesomethingdaily.com/wp-content/uploads/2015/02/profile-picture-circle.png',
+'http://static1.squarespace.com/static/5491d7aae4b0d06755583522/t/54af31ffe4b0ce916d639133/1420767744829/Laura+Farrelly+Profile+Photo+Circle.png',
+'http://ryan-brogan.com/wp-content/uploads/2014/03/Profile-circle.png',
+'http://www.noviventure.com/wp-content/uploads/2014/08/Profile-Picture-Circle-Transparent.png',
+'http://www.thingsworthdescribing.com/wp-content/uploads/2014/06/edited-profile-pic-circle.png',
+'http://shaunkutch.com/wp-content/uploads/2015/02/circle_profile_photo.png'
+];
+
 
 
 myHandler = function() {
@@ -55,11 +63,11 @@ myHandler = function() {
       data: {query: $("#q").val()}
     }).done(function(data) {
       if (data['suggestions'].length != 0) {
+        $('#results').toggle();
         $('#results').empty();
-        $('#results').append('<ul id="results_list"></ul>');
-        var x = document.getElementById('results_list');
-        x.style.zIndex = 100000;
+
         var sugg = data['suggestions'];
+        var i = face_array.length - 1;
         sugg.forEach( function (s) {
             var link = '';
             if (s["type"] == "dish") {
@@ -69,10 +77,9 @@ myHandler = function() {
               link = '/users/'+s['id'] + '/show';
             }
             console.log(link);
-
-            $('#results_list').append('<li><a style="text-decoration: none;" href="'+link+'">'+ s['name'] +'</a></li>');
+            $('#results').append('<a class="special_a" style="text-decoration: none;" href="'+link+'">'+ s['name'] + '<img style="position: relative; float:right; top: 5px;" height="50px" src="'+ face_array[i]+'">' + '</a>');
+            i -= 1;
         });
-
       }
     });    
   }
