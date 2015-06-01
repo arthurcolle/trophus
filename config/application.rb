59f82b785ea8eb79c0fad806eb189de29e47e7fe
Arthur Colle <hello@arthurcolle.com>
2015-06-01 17:01:26 -0400

    Changed assets version

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module Trophus
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.enabled = true
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
   end
 end
