78056242e60d97ba8f4db5e42f3d29572e2442e1
Arthur Colle <hello@arthurcolle.com>
2015-06-02 19:52:09 -0400

    Search is location based now

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,9 +5,28 @@ class SearchController < ApplicationController
 	  if params["query"].nil?
 	    @using = []
 	  else
-	    a = User.ac_search(params["query"]).to_json
-	   	d = Dish.ac_search(params["query"]).to_json
+	  	beyond = User.beyond(1, :origin => current_user)
+	  	puts "Beyond #{current_user.name}"
+	  	puts beyond
+	  	ids_not = []
+	  	beyond.each { |dude| 
+	  		ids_not.push(dude.id)
+	  	}
 
+	  	dishes_not = []
+	  	ids_not.each {|id| 
+	  		dishes = User.find(id).dishes
+	  		dishes.each {|dish|
+	  			dishes_not.push(dish.id)
+	  		}
+	  	}
+
+	    a = User.ac_search(params["query"], without: { id: ids_not}).to_json
+	    puts "Users returned"
+	    puts a
+	   	d = Dish.ac_search(params["query"], without: {id: dishes_not}).to_json
+	   	puts "Dishes returned"
+	   	puts d
 	   	user_list = JSON.parse(a)
 	   	dish_list = JSON.parse(d)
 	   	listx = []
@@ -32,12 +51,11 @@ class SearchController < ApplicationController
 	   		item = {:type=> "dish", :id => did, :score => dscore, :name => dname, :description => ddesc, :price => dp}
 	   		dlist.push(item)
 	   		listx.push(item)
-
 	   	}
 
-	   	listy = listx.sort_by{|h| h["score"]}
-	   	puts listy
 
+	   	listy = listx.sort_by{|h| h["score"]}
+		puts listy
 	   	
 	  end
 	  puts @using
