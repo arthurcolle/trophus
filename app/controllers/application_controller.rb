fe22e27e5806b793ad8a57ba908a3a07d26c2eb3
Arthur Colle <hello@arthurcolle.com>
2015-06-03 22:09:31 -0400

    Added mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,20 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   #   User.current_user = nil
   # end  
 
+  def check_for_mobile
+    session[:mobile_override] = params[:mobile] if params[:mobile]
+  end
+
+  def mobile_device?
+    if session[:mobile_override]
+      session[:mobile_override] == "1"
+    else
+      # Season this regexp to taste. I prefer to treat iPad as non-mobile.
+      (request.user_agent =~ /Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/) && (request.user_agent !~ /iPad/)
+    end
+  end
+  helper_method :mobile_device?
+
   
   protected
 
