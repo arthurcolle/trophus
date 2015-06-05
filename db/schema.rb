e0b5536d7d44c25767cf29e666e47b9cc317e70b
Arthur Colle <hello@arthurcolle.com>
2015-06-04 20:03:44 -0400

    Master merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150601185731) do
+ActiveRecord::Schema.define(version: 20150604231936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,11 @@ ActiveRecord::Schema.define(version: 20150601185731) do
 
   create_table "residences", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reviews", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
