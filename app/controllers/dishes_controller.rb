7db7d41cfa4d0643282a09343ba566f636df88ae
Arthur Colle <hello@arthurcolle.com>
2015-06-06 23:46:31 -0400

    Try fix image_url

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -67,10 +67,6 @@ class DishesController < ApplicationController
 	end
 
 	private
-    # Using a private method to encapsulate the permissible parameters
-    # is just a good pattern since you'll be able to reuse the same
-    # permit list between create and update. Also, you can specialize
-    # this method with per-user checking of permissible attributes.
     def dish_params
     	if !params["image_url"].nil?
 			params["image"] = Dish.picture_from_url(params["image_url"])
