4fa93b0514b41214811848ab832d11f33883a63a
Arthur Colle <hello@arthurcolle.com>
2015-06-08 11:28:13 -0400

    Maybe will fix

--- app/assets/javascripts/listeners.js
+++ app/assets/javascripts/listeners.js
@@ -1,3 +1,38 @@
+
+function addStripeInformation(data) {
+  var handler = StripeCheckout.configure({
+    key: 'pk_test_k90DPHCGKmfYhYa5anVRrVKy',
+    token: function(token) {
+      $.ajax({
+        url: '/charges/create',
+        type: "POST",
+        data: {
+          "token" : token.id,
+          "email" : data.email
+        }
+      });
+    }
+  });
+
+  $(function(){
+    // Open Checkout with further options
+    handler.open({
+      email: data.email,
+      name: data.name,
+      description: 'You\'ll be eating before you know it',
+      zipCode: false,
+      panelLabel: "Add Information",
+      allowRememberMe: false
+    });
+  });
+
+  // Close Checkout on page navigation
+  $(window).on('popstate', function() {
+    handler.close();
+  });
+}
+
+
 $(function() {
   /* event listener for ADD CARD button click */
 	$("#addcard").click(function() {
@@ -14,7 +49,6 @@ $(function() {
 
 	$('#change-prof-pic').click(function() {
 		profChanger();
-	});
-	
+	});	
 });
 
