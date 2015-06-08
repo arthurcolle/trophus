e88f2d1c5a2873964fb39e6c40b93601602a0e5c
Arthur Colle <hello@arthurcolle.com>
2015-06-08 11:38:58 -0400

    Removed assets

--- app/assets/javascripts/listeners.js
+++ app/assets/javascripts/listeners.js
@@ -1,41 +1,6 @@
-
-function addStripeInformation(data) {
-  var handler = StripeCheckout.configure({
-    key: 'pk_test_k90DPHCGKmfYhYa5anVRrVKy',
-    token: function(token) {
-      $.ajax({
-        url: '/charges/create',
-        type: "POST",
-        data: {
-          "token" : token.id,
-          "email" : data.email
-        }
-      });
-    }
-  });
-
-  $(function(){
-    // Open Checkout with further options
-    handler.open({
-      email: data.email,
-      name: data.name,
-      description: 'You\'ll be eating before you know it',
-      zipCode: false,
-      panelLabel: "Add Information",
-      allowRememberMe: false
-    });
-  });
-
-  // Close Checkout on page navigation
-  $(window).on('popstate', function() {
-    handler.close();
-  });
-}
-
-
 $(function() {
   /* event listener for ADD CARD button click */
-	$("#addcard").click(function() {
+	$("#addcard").on("click", function() {
 		var name = $('#addcard').attr('data-name');
 		var email = $('#addcard').attr('data-email');
 		addStripeInformation({'name' : name, 'email' : email});
@@ -43,12 +8,12 @@ $(function() {
 
 
   /* event listener for ADD BANK ACCOUNT button click */
-	$('#addbank').click(function() {
+	$('#addbank').on("click", function() {
 		$('#modal1').openModal();
 	});
 
-	$('#change-prof-pic').click(function() {
+	$('#change-prof-pic').on('click', function() {
 		profChanger();
-	});	
+	});
 });
 
