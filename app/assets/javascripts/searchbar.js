6fe663bfe09005786ca785006a33a67afe18bf48
Arthur Colle <hello@arthurcolle.com>
2015-06-08 19:22:57 -0400

    Small search fix

--- app/assets/javascripts/searchbar.js
+++ app/assets/javascripts/searchbar.js
@@ -17,20 +17,22 @@ myHandler = function() {
       data: {query: $("#q").val()}
     }).done(function(data) {
       $('#results').show();
-      if (data['suggestions'].length != 0) {
-        $('#results').empty();
-        var sugg = data['suggestions'];
-        sugg.forEach( function (s) {
-            var link = '';
-            if (s["type"] == "dish") {
-              link = '/dishes/'+s['id'];
-            }
-            else {
-              link = '/users/'+s['id'] + '/show';
-            }
-            console.log(link);
-            $('#results').append('<a class="special_a" style="text-decoration: none; " href="'+link+'">'+ s['name'] + '<img alt="" style="position: relative; float:right; top: 5px; border-radius: 5px 5px 5px 5px" height="50px" src="'+ s['pic'] +'">' + '</a>');              
-        });
+      if (data['suggestions'] !== undefined) {
+        if (data['suggestions'].length !== 0) {
+          $('#results').empty();
+          var sugg = data['suggestions'];
+          sugg.forEach( function (s) {
+              var link = '';
+              if (s["type"] == "dish") {
+                link = '/dishes/'+s['id'];
+              }
+              else {
+                link = '/users/'+s['id'] + '/show';
+              }
+              console.log(link);
+              $('#results').append('<a class="special_a" style="text-decoration: none; " href="'+link+'">'+ s['name'] + '<img alt="" style="position: relative; float:right; top: 5px; border-radius: 5px 5px 5px 5px" height="50px" src="'+ s['pic'] +'">' + '</a>');              
+          });
+        }        
       }
     });    
   }
