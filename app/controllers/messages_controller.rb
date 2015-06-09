6dfa507af1854a57fdecf8c3f0bde87afa057a60
Arthur Colle <hello@arthurcolle.com>
2015-06-09 02:42:02 -0400

    Fixed notify

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
  
   def create
     recipient = User.find(params[:user])
-    recipient.notify("#{current_user.name} sent you a message!", params[:message][:subject])
+    recipient.notify("#{current_user.name} sent you a message!", "xyz")
     conversation = current_user.send_message(recipient, params[:message][:body], params[:message][:subject]).conversation
     flash[:success] = "Message has been sent!"
     redirect_to conversation_path(conversation)
