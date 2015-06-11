e58a5cfa03f7194ce97e0e75c0c21d9bcde42283
Arthur Colle <hello@arthurcolle.com>
2015-06-11 00:20:37 -0400

    Added notifications

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,13 +1,13 @@
-//function fetchNotifs() {
-//	$.ajax({
-//		type: "GET",
-//		url: 'get_user_notif_count'
-//	});
-//}
-//
-//$(function() {
-//	setInterval(fetchNotifs, 1000);
-//});
+function fetchNotifs() {
+	$.ajax({
+		type: "GET",
+		url: 'get_user_notif_count'
+	});
+}
+
+$(function() {
+	setInterval(fetchNotifs, 1000);
+});
 
 $(function() {
     if (window.location.protocol != "https:")
