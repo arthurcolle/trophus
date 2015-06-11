7395bd66a33aa82397d4022554e5c043e54ab568
Arthur Colle <hello@arthurcolle.com>
2015-06-11 17:45:55 -0400

    Always forget the damn https

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,15 +1,15 @@
-//function fetchNotifs() {
-//$.ajax({
-//    type: "GET",
-//    url: 'get_user_notif_count'
-//});
-//}
-//
-//$(function() {
-//    setInterval(fetchNotifs, 1000);
-//});
-//
-//$(function() {
-//    if (window.location.protocol != "https:")
-//        window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
-//})
+function fetchNotifs() {
+$.ajax({
+    type: "GET",
+    url: 'get_user_notif_count'
+});
+}
+
+$(function() {
+    setInterval(fetchNotifs, 1000);
+});
+
+$(function() {
+    if (window.location.protocol != "https:")
+        window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
+})
