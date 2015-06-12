a363a33be72ede208e71f9371a2b14fcb51a3119
Arthur Colle <hello@arthurcolle.com>
2015-06-12 18:35:13 -0400

    Fixed footer on mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
-before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   skip_before_filter :require_login, :except=>[]
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
 
   protect_from_forgery with: :null_session
   helper_method :current_order
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,15 +7,15 @@ class MessagesController < ApplicationController
  
   def create
     recipient = User.find(params[:user])
-    recipient.notify("#{current_user.name} sent you a message!", "xyz")
-    conversation = current_user.send_message(recipient, params[:message][:body], params[:message][:subject]).conversation
+    recipient.notify("#{current_user.name} sent you a message!", "Filler");
+    msg_body = params[:message][:body]
+    msg_subj = params[:message][:subject]
+    conversation = current_user.send_message(recipient, msg_body, msg_subj).conversation
     flash[:success] = "Message has been sent!"
     redirect_to conversation_path(conversation)
+    # naming convention for server-client channels is 'private-<%= id %>'
     channel = 'private-' + recipient.id.to_s
-    puts "CHANNEL"
-    @a = current_user.name
-    @b = recipient.name
-    puts channel
+    # push notification to trigger the
     Pusher[channel].trigger('my_event', {
       message: '1 new message'
     })
