c670ec509ec1d2a56f8430ae152a63420e0baa0e
Arthur Colle <hello@arthurcolle.com>
2015-06-14 05:16:33 -0400

    Fixed initialize for s3 direct

--- config/initializers/s3_direct_upload.rb
+++ config/initializers/s3_direct_upload.rb
@@ -1,7 +1,7 @@
 S3DirectUpload.config do |c|
   c.access_key_id = ENV['AWS_ACCESS_KEY_ID']       		# your access key id
   c.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']    # your secret access key
-  c.bucket = "colle-superduper"              # your bucket name
+  c.bucket = "ENV['S3_BUCKET']"              # your bucket name
   c.url = "https://#{c.bucket}.s3.amazonaws.com/"
 end
 
